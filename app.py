8867e7fa0e7fc6917d24ae0d10a79f65abac1f50
JP3tri3 <JP3tri3@gmail.com>
2021-02-28 15:57:35 -0500

    requirements commit

---
+++ app.py
@@ -0,0 +1,11 @@
+import config
+from time import time, sleep
+import json
+import time
+import bybit
+import binance_info
+from sanic import Sanic
+from sanic import response
+from sanic.request import Request
+from sanic.response import json
+from sanic_jinja2 import SanicJinja2
---
+++ config.py
@@ -0,0 +1,2 @@
+Bybit_TestNet_API_KEY = "ZuEJ0HIkfMzcntVY7r"
+Bybit_TestNet_API_SECRET = "Q5DxESJyfkptCPJcKmKD0usrex3DFuR052Kc"
