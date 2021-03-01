03920fe5f588151bbd19a3be75e6823af9e65f21
JP3tri3 <57971582+JP3tri3@users.noreply.github.com>
2021-03-01 01:16:29 -0500

    still working on it

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ client = bybit.bybit(test=True, api_key=config.BYBIT_TESTNET_API_KEY,
                      api_secret=config.BYBIT_TESTNET_API_SECRET)
 
 flag = True
+orderId = ""
 
 
 def timeStamp():
@@ -29,27 +30,48 @@ def timeStamp():
     print("Time: ", ct)
 
 
+def shutdown():
+    print("")
+    print("Shutting down...")
+    sys.exit("Program Terminated")
+    print("")
+
+
 def inputOptions():
     print("")
     print("Input Options:")
-    print("Create Long Order: 'buy'")
+    print("Create Long Order: 'long'")
     print("BTC Price info: 'btc price'")
     print("BTC Info: 'btc info'")
     print("BTC Wallet: 'btc wallet'")
     print("Eth Wallet: 'eth wallet'")
+    print("Active Orders: 'active order'")
+    print("Position: 'position'")
+    print("Cancel Orders: 'cancel orders'")
+    print("Stop Loss: 'stoploss'")
+    print("Market Close: 'close'")
+    print("Order Id: 'order id'")
     print("Exit: 'exit'")
 
 
-def placeOrder(side, symbol, order_type, price):
-    try:
-        print(f"sending order {price} - {side} {symbol} {order_type}")
-        order = client.Order.Order_new(side=side, symbol=symbol, order_type=order_type,
-                                       qty=1, price=price, time_in_force="GoodTillCancel").result()
+def cancelOrder(orderId, symbol):
+    client.LinearOrder.LinearOrder_cancel(
+        symbol="BTCUSD", order_id=orderId).result()
 
+
+def placeLongOrder(side, symbol, order_type, price):
+    global orderId
+    stop_loss = bybit_info.btcLastPrice() - 400
+    try:
+        print(
+            f"sending order {price} - {side} {symbol} {order_type} {stop_loss}")
+        order = client.Order.Order_new(side=side, symbol=symbol, order_type=order_type,
+                                       qty=1, price=price, time_in_force="PostOnly", stop_loss=stop_loss).result()
+        orderId = str(order[0]['result']['order_id'])
+        print(orderId)
     except Exception as e:
         print("an exception occured - {}".format(e))
         return False
-
     return order
 
 
@@ -65,8 +87,7 @@ def main():
         timeStamp()
 
         if(taskInput == "exit"):
-            print("Shutting down...")
-            sys.exit("Program Terminated")
+            shutdown()
 
         elif(taskInput == "btc price"):
             bybit_info.btcPriceInfo()
@@ -74,9 +95,9 @@ def main():
         elif(taskInput == "btc info"):
             bybit_info.btcInfo()
 
-        elif(taskInput == "buy"):
-            order_response = placeOrder(
-                "Buy", "BTCUSD", "Limit", bybit_info.btcLastPrice())
+        elif(taskInput == "long"):
+            order_response = placeLongOrder(
+                "Buy", "BTCUSD", "Limit", bybit_info.btcLastPrice() - 0.50)
             if order_response:
                 print("Order Successful")
             else:
@@ -87,6 +108,28 @@ def main():
 
         elif(taskInput == "eth wallet"):
             bybit_info.ethWallet()
+
+        elif(taskInput == "active order"):
+            print(bybit_info.activeOrder())
+
+        elif(taskInput == "stoploss"):
+            bybit_info.changeStopLoss("BTCUSD", 500)
+            print("Updated Stop Loss")
+
+        elif(taskInput == "close"):
+            bybit_info.changeStopLoss("BTCUSD", 10)
+            print("Updated Stop Loss")
+
+        elif(taskInput == "cancel orders"):
+            bybit_info.cancelAllOrders("BTCUSD")
+            print("Orders Cancelled")
+
+        elif(taskInput == "order id"):
+            print("Order ID: " + orderId)
+
+        elif(taskInput == "position"):
+            print("Position: ")
+            bybit_info.myPosition()
 
         else:
             print("Invalid Input, try again...")
--- bybit_info.py
+++ bybit_info.py
@@ -1,29 +1,30 @@
 import bybit
 import config
+import json
 
 client = bybit.bybit(test=True, api_key=config.BYBIT_TESTNET_API_KEY,
                      api_secret=config.BYBIT_TESTNET_API_SECRET)
 
-info = client.Market.Market_symbolInfo().result()
-keys = info[0]['result']
+# info = client.Market.Market_symbolInfo().result()
+# keys = info[0]['result']
 
-btcOrderBookInfo = client.Market.Market_orderbook(symbol="BTCUSD").result()
+# btcOrderBookInfo = client.Market.Market_orderbook(symbol="BTCUSD").result()
 
 # inverse perpetual keys
-btcInfo = keys[0]
-ethInfo = keys[1]
-eosInfo = keys[2]
-xrpInfo = keys[3]
+# btcInfo = keys[0]
+# ethInfo = keys[1]
+# eosInfo = keys[2]
+# xrpInfo = keys[3]
 
-# usdt perpetual keys
-btcusdtInfo = keys[4]
-ethusdtInfo = keys[5]
-ltcusdtInfo = keys[6]
-linkusdtInfo = keys[7]
-xtzusdtInfo = keys[8]
+# # usdt perpetual keys
+# btcusdtInfo = keys[4]
+# ethusdtInfo = keys[5]
+# ltcusdtInfo = keys[6]
+# linkusdtInfo = keys[7]
+# xtzusdtInfo = keys[8]
 
-# Price Information:
-ethLastPrice = ethInfo['last_price']
+# # Price Information:
+# ethLastPrice = ethInfo['last_price']
 
 
 # eth Balance in USD
@@ -76,3 +77,38 @@ def btcInfo():
     keys = info[0]['result']
     btcInfo = keys[0]
     print(btcInfo)
+
+
+# def btcActiveOrder():
+#     activeOrder = client.LinearOrder.LinearOrder_getOrders(
+#         symbol="BTCUSD").result()
+#     activeOrderResult = activeOrder[0]['result']
+#     return activeOrderResult
+
+def activeOrder():
+    activeOrder = client.Order.Order_query(symbol="BTCUSD").result()
+    # print(activeOrder)
+    order = activeOrder[0]['result']
+    if (order == []):
+        return 0
+    else:
+        return 1
+
+
+def cancelAllOrders(symbol):
+    client.Order.Order_cancelAll(symbol=symbol).result()
+
+
+# def cancelOrder(symbol, orderId):
+#     client.Order.Order_cancel(symbol=symbol, order_id=orderId).result()
+
+
+def myPosition():
+    position = client.Positions.Positions_myPosition(symbol="BTCUSD").result()
+    print(position)
+
+
+def changeStopLoss(symbol, subtract):
+    stop_loss = str(btcLastPrice() - subtract)
+    client.Positions.Positions_tradingStop(
+        symbol=symbol, stop_loss=stop_loss).result()
